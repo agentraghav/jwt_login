c2a9996467fda1658160c8723de19d8d59e6efbf
agentraghav <raghvendrarathore2@gmail.com>
2021-02-26 12:22:06 +0530

    server setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => console.log(`Server connected on PORT ${PORT}`));
+
 mongoose.connect(
   process.env.MONGOOSE_CONNECTION_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },

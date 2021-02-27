85866b046fd850f85119c904a885507d7cf21586
agentraghav <raghvendrarathore2@gmail.com>
2021-02-28 00:35:22 +0530

    setup client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import axios from 'axios';
+
+import Header from './components/header';
+import Home from './components/home';
+import Register from './components/register';
+import Login from './components/login';
+import UserContext from './context/userContext';
 import './App.css';
 
 function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem('auth-token');
+      if (token === null) {
+        localStorage.setItem('auth-token', '');
+        token = '';
+      }
+
+      const tokenResponse = await axios.post(
+        'http://localhost:5000/users/tokenIsValid',
+        null,
+        { headers: { 'x-auth-token': token } }
+      );
+
+      if (tokenResponse.data) {
+        const userRes = await axios.get('http://localhost:5000/users/', {
+          headers: { 'x-auth-token': token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <Header />
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
